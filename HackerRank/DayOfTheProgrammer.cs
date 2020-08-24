802ad06327b32142d9c833caec471495b29227ad
Donovan Beck <donovan.beck.69@outlook.com>
2020-08-24 14:13:59 -0700

    Day of Programmer

---
+++ HackerRank/DayOfTheProgrammer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace HackerRank
+{
+    public class DayOfTheProgrammer
+    {
+        public string dayOfProgrammer(int year)
+        {
+            string day = string.Empty;
+            DateTime pdate = new DateTime(year, 1, 1); //, new GregorianCalendar()
+            int daysToAdd = 255; 
+
+            if(year == 1918)
+            {
+                daysToAdd += 13;
+            }
+            else if (year <= 1917)
+            {
+                // test for julian leap year
+                if (DateTime.IsLeapYear(year) == false && year % 4 == 0)
+                {
+                    daysToAdd--;
+                }
+            }
+
+            pdate = pdate.AddDays(daysToAdd);
+            day = pdate.ToString("dd.MM.yyyy");
+
+            return day;
+
+        }
+    }
+}
---
+++ HackerTests/DayOfTheProgrammerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HackerRank;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Tests
+{
+    [TestClass()]
+    public class DayOfTheProgrammerTests
+    {
+        [TestMethod()]
+        public void dayOfProgrammerTest()
+        {
+            DayOfTheProgrammer dayOfTheProgrammer = new DayOfTheProgrammer();
+
+            string day = dayOfTheProgrammer.dayOfProgrammer(2017);
+            Assert.IsTrue(day == "13.09.2017");
+        }
+
+        [TestMethod()]
+        public void dayOfProgrammerTest2()
+        {
+            DayOfTheProgrammer dayOfTheProgrammer = new DayOfTheProgrammer();
+
+            string day = dayOfTheProgrammer.dayOfProgrammer(2016);
+            Assert.IsTrue(day == "12.09.2016");
+        }
+
+        [TestMethod()]
+        public void dayOfProgrammerTest3()
+        {
+            DayOfTheProgrammer dayOfTheProgrammer = new DayOfTheProgrammer();
+
+            string day = dayOfTheProgrammer.dayOfProgrammer(1800);
+            Assert.IsTrue(day == "12.09.1800");
+        }
+
+        [TestMethod()]
+        public void dayOfProgrammerTest4()
+        {
+            DayOfTheProgrammer dayOfTheProgrammer = new DayOfTheProgrammer();
+
+            string day = dayOfTheProgrammer.dayOfProgrammer(1804);
+            Assert.IsTrue(day == "13.09.2002");
+        }
+
+        [TestMethod()]
+        public void dayOfProgrammerTest5()
+        {
+            DayOfTheProgrammer dayOfTheProgrammer = new DayOfTheProgrammer();
+
+            string day = dayOfTheProgrammer.dayOfProgrammer(1916);
+            Assert.IsTrue(day == "12.09.1916");
+        }
+
+        [TestMethod()]
+        public void dayOfProgrammerTest6()
+        {
+            DayOfTheProgrammer dayOfTheProgrammer = new DayOfTheProgrammer();
+
+            string day = dayOfTheProgrammer.dayOfProgrammer(1918);
+            Assert.IsTrue(day == "26.09.1918");
+        }
+
+
+    }
+}
