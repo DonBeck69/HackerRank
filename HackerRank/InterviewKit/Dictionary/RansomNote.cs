20ec8df2a5390ac505361c24aaadbc9fdc68c799
Donovan Beck <donovan.beck.69@outlook.com>
2020-08-31 16:40:17 -0700

    Hashmaps

---
+++ HackerRank/InterviewKit/Dictionary/RansomNote.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.InterviewKit.Dictionary
+{
+    public class RansomNote
+    {
+        public string checkMagazine(string[] magazine, string[] note)
+        {
+            string result = "Yes";
+
+            Dictionary<string, int> mags = BuildDictionary(magazine);
+            Dictionary<string, int> notes = BuildDictionary(note);
+
+            foreach(KeyValuePair<string, int> word in notes)
+            {
+                if ((mags.ContainsKey(word.Key) && mags[word.Key] >= word.Value) == false)
+                {
+                    result = "No";
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private Dictionary<string, int> BuildDictionary(string[] arr)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            for(int index = 0; index <= arr.Length - 1; index++)
+            {
+                if(result.ContainsKey(arr[index]))
+                {
+                    result[arr[index]]++;
+                }
+                else
+                {
+                    result.Add(arr[index], 1);
+                }
+            }
+
+            return result;
+        }
+
+
+        public string checkMagazineBrute(string[] magazine, string[] note)
+        {
+            string result = "Yes";
+            List<string> magazines = new List<string>(magazine);
+            List<string> notes = new List<string>(note);
+            foreach (string n in notes)
+            {
+                if (magazines.Contains(n))
+                {
+                    magazines.Remove(n);
+                }
+                else
+                {
+                    result = "No";
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ HackerTests/InterviewKit/Dictionary/RansomNoteTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HackerRank.InterviewKit.Dictionary;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using HackerRank;
+
+namespace HackerRank.InterviewKit.Dictionary.Tests
+{
+    [TestClass()]
+    public class RansomNoteTests
+    {
+        [TestMethod()]
+        public void checkMagazineTest()
+        {
+            RansomNote rn = new RansomNote();
+            string[] mag = "give me one grand today night".Split(' ');
+            string[] note = "give one grand today".Split(' ');
+
+            string res = rn.checkMagazine(mag, note);
+            Assert.IsTrue(res == "Yes");
+        }
+
+        [TestMethod()]
+        public void checkMagazineTest2()
+        {
+            RansomNote rn = new RansomNote();
+            string[] mag = "two times three is not four".Split(' ');
+            string[] note = "two times two is four".Split(' ');
+
+            string res = rn.checkMagazine(mag, note);
+            Assert.IsTrue(res == "No");
+        }
+
+        [TestMethod()]
+        public void checkMagazineTest3()
+        {
+            RansomNote rn = new RansomNote();
+            string[] mag = "ive got some lovely bunch of coconuts".Split(' ');
+            string[] note = "ive got some Coconuts".Split(' ');
+
+            string res = rn.checkMagazine(mag, note);
+            Assert.IsTrue(res == "No");
+        }
+
+        [TestMethod()]
+        public void checkMagazineTest10()
+        {
+            string[] lines = File.ReadAllLines(@"./Ransom/input10.txt");
+            string[] mag = lines[1].Split(' ');
+            string[] note = lines[2].Split(' ');
+
+            RansomNote rn = new RansomNote();
+            string res = rn.checkMagazine(mag, note);
+            Assert.IsTrue(res == "No");
+        }
+
+
+    }
+}
