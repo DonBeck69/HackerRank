e60b72b366d4ce553a410eaf44dc6af1a91b836a
Donovan Beck <donovan.beck.69@outlook.com>
2020-09-29 16:56:48 -0700

    Traversing the tree, swap nodes

---
+++ HrNet/Interview/Search/SwapNodes.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrNet.Interview.Search
+{
+    public class SwapNodes
+    {
+        public  int[][] swapNodes(int[][] indexes, int[] queries)
+        {
+            int[][] res = new int[queries.Length][];
+            Node node = GetTree(indexes);
+            // now do the swaps
+            for(int index = 0; index <= queries.Length - 1; index++)
+            {
+                SwapNodesAt(node, queries[index], 1);
+                // walk the tree for results
+                List<int> result = new List<int>();
+                TraverseTree(node, result);
+                res[index] = result.ToArray();
+            }
+
+
+            return res;
+        }
+
+        /// <summary>
+        /// recursivly walk tree, at given level swap child nodes.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="swap"></param>
+        /// <param name="level"></param>
+        public void SwapNodesAt(Node node, int swap, int level)
+        {
+            if (level % swap == 0)
+            {
+                Node left = node.Left;
+                node.Left = node.Right;
+                node.Right = left;
+            }
+
+            level++;
+            if (node.Left != null)
+            {
+                SwapNodesAt(node.Left, swap, level);
+            }
+
+            if (node.Right != null)
+            {
+                SwapNodesAt(node.Right, swap, level);
+            }
+        }
+
+        /// <summary>
+        /// return a integer list from the in order tree traversal
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="Result"></param>
+        public void TraverseTree(Node node, List<int> Result)
+        {
+            if (node.Left != null)
+            {
+                TraverseTree(node.Left, Result);
+            }
+            Result.Add(node.index);
+            if (node.Right != null)
+            {
+                TraverseTree(node.Right, Result);
+            }
+            
+        }
+
+        /// <summary>
+        /// kick off the tree build, initialize the root and then call the recursive function
+        /// </summary>
+        /// <param name="Indexes"></param>
+        /// <returns></returns>
+        public Node GetTree(int[][] Indexes)
+        {
+            Node node = new Node() { index = 1 };
+            if (Indexes[0][0] > -1)
+            {
+                node.Left = AddNode(Indexes[0][0], Indexes);
+            }
+
+            if (Indexes[0][1] > -1)
+            {
+
+                node.Right = AddNode(Indexes[0][1], Indexes);
+            }
+
+            return node;
+        }
+
+        /// <summary>
+        /// build the tree recursivly from the input int[][]
+        /// </summary>
+        /// <param name="Index"></param>
+        /// <param name="Indexes"></param>
+        /// <returns></returns>
+        public Node AddNode(int Index, int[][] Indexes)
+        {
+            Node node = new Node() { index = Index };
+            if (Indexes[Index - 1][0] > -1)
+            {
+                node.Left = AddNode(Indexes[Index - 1][0], Indexes);
+            }
+
+            if (Indexes[Index - 1][1] > -1)
+            {
+                node.Right = AddNode(Indexes[Index - 1][1], Indexes);
+            }
+
+            return node;
+        }
+    }
+
+
+
+
+    public class Node
+    {
+        public int index;
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+    }
+}
+
+
---
+++ HrNetTests/Interview/Search/SwapNodesTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HrNet.Interview.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrNet.Interview.Search.Tests
+{
+    [TestClass()]
+    public class SwapNodesTests
+    {
+        [TestMethod()]
+        public void swapNodesTest0()
+        {
+            SwapNodes swapNodes = new SwapNodes();
+            int[][] indexes = new int[3][];
+            indexes[0] = new int[2] { 2, 3 };
+            indexes[1] = new int[2] { -1, -1 };
+            indexes[2] = new int[2] { -1, -1 };
+            int[] qs = new int[2] { 1, 1 };
+            int[][] res = swapNodes.swapNodes(indexes, qs);
+
+            Assert.IsTrue(string.Join(",", res[0]) == "3,1,2");
+            Assert.IsTrue(string.Join(",", res[1]) == "2,1,3");
+        }
+
+
+        [TestMethod()]
+        public void swapNodesTest1()
+        {
+            SwapNodes swapNodes = new SwapNodes();
+            int[][] indexes = new int[5][];
+            indexes[0] = new int[2] { 2, 3 };
+            indexes[1] = new int[2] { -1, 4 };
+            indexes[2] = new int[2] { -1, 5 };
+            indexes[3] = new int[2] { -1, -1 };
+            indexes[4] = new int[2] { -1, -1 };
+            int[] qs = new int[1] { 2 };
+            int[][] res = swapNodes.swapNodes(indexes, qs);
+
+            Assert.IsTrue(string.Join(",", res[0]) == "4,2,1,5,3");
+            //Assert.IsTrue(string.Join(",", res[1]) == "2,1,3");
+        }
+
+
+        [TestMethod()]
+        public void swapNodesTest2()
+        {
+            SwapNodes swapNodes = new SwapNodes();
+            int[][] indexes = new int[11][];
+            indexes[0] = new int[2] { 2, 3 };
+            indexes[1] = new int[2] { 4, -1 };
+            indexes[2] = new int[2] { 5, -1 };
+            indexes[3] = new int[2] { 6, -1 };
+            indexes[4] = new int[2] { 7, 8 };
+            indexes[5] = new int[2] { -1, 9 };
+            indexes[6] = new int[2] { -1, -1 };
+            indexes[7] = new int[2] { 10, 11};
+            indexes[8] = new int[2] { -1, -1 };
+            indexes[9] = new int[2] { -1, -1 };
+            indexes[10] = new int[2] { -1, -1 };
+            int[] qs = new int[2] { 2, 4 };
+            int[][] res = swapNodes.swapNodes(indexes, qs);
+
+            Assert.IsTrue(string.Join(", ", res[0]) == "2, 9, 6, 4, 1, 3, 7, 5, 11, 8, 10");
+            Assert.IsTrue(string.Join(", ", res[1]) == "2, 6, 9, 4, 1, 3, 7, 5, 10, 8, 11");
+        }
+
+    }
+}
