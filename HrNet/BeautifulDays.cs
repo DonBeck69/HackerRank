6d7a16df4a5b33c503d6c4009dab014053feaeee
Donovan Beck <donovan.beck.69@outlook.com>
2020-12-17 10:43:20 -0800

    Beautifull days

---
+++ HrNet/BeautifulDays.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrNet
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
+    /// </summary>
+    public class BeautifulDays
+    {
+        public  int beautifulDays(int i, int j, int k)
+        {
+            int res = 0;
+            for (int n = i; n <= j; n++)
+            {
+                //reverse the number
+                int reverse = ReverseInt(n);
+                // get absolute diff
+                int diff = Math.Abs(n - reverse);
+                //test if clean divide by k
+                if (diff % k == 0)
+                {
+                    res++;
+                }
+            }
+            return res;
+        }
+
+        public int ReverseInt(int number)
+        {
+            int reverse = 0;
+            int temp = number;
+            while(Convert.ToBoolean(temp))
+            {
+                int remainder = temp % 10;
+                reverse = reverse * 10 + remainder;
+                temp = temp / 10;
+            }
+
+            return reverse;
+        }
+    }
+}
---
+++ HrNetTests/BeutifulDaysTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HrNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrNet.Tests
+{
+    [TestClass()]
+    public class BeutifulDaysTests
+    {
+
+        //test the reverse function
+        [TestMethod()]
+        public void ReverseIntTest()
+        {
+            BeautifulDays beautifulDays = new BeautifulDays();
+            int res = beautifulDays.ReverseInt(1234);
+            Assert.IsTrue(res == 4321);
+        }
+
+        [TestMethod()]
+        public void ReverseIntTest1()
+        {
+            BeautifulDays beautifulDays = new BeautifulDays();
+            int res = beautifulDays.ReverseInt(1230);
+            Assert.IsTrue(res == 321);
+        }
+
+        [TestMethod()]
+        public void ReverseIntTest2()
+        {
+            BeautifulDays beautifulDays = new BeautifulDays();
+            int res = beautifulDays.ReverseInt(1003);
+            Assert.IsTrue(res == 3001);
+        }
+
+        //test the beautifill days function
+        [TestMethod()]
+        public void beautifulDaysTest1()
+        {
+            BeautifulDays beautifulDays = new BeautifulDays();
+            int res = beautifulDays.beautifulDays(20, 23, 6);
+            Assert.IsTrue(res == 2);
+        }
+    }
+}
